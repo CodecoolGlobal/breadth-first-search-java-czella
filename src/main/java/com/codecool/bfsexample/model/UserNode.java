715a14aac7ed8fcb47cd966ca6206c103a54da8d
Laszlo Czellaho <laszlo.czellaho@gmail.com>
2019-02-27 11:35:03 +0100

    Add BFS method and functional solution for finding distance

--- src/main/java/com/codecool/bfsexample/model/UserNode.java
+++ src/main/java/com/codecool/bfsexample/model/UserNode.java
@@ -1,6 +1,8 @@
 package com.codecool.bfsexample.model;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class UserNode {
@@ -50,5 +52,21 @@ public class UserNode {
 
     public String toString() {
         return firstName + "_" + lastName + " (" + id + ")";
+    }
+
+    public boolean isIn(List<UserNode> list) {
+
+        Iterator<UserNode> it = list.iterator();
+        while (it.hasNext()) {
+
+            UserNode current = it.next();
+
+            if (current.getId() == this.getId()) {
+                return true;
+            }
+
+        }
+        return false;
+
     }
 }
